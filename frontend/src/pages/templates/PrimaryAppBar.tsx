d7c88a904c915108cd458630411495b8d58e8b8e
Feyzullah <f.erkuysal@gmail.com>
2024-07-27 19:38:02 +0300

    Setting up Drawer

--- frontend/src/pages/templates/PrimaryAppBar.tsx
+++ frontend/src/pages/templates/PrimaryAppBar.tsx
@@ -1,11 +1,28 @@
-import {AppBar, Link, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Drawer, IconButton, Link, MenuItem, Toolbar, Typography, useMediaQuery} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 import {useTheme} from "@mui/material/styles";
+import {useEffect, useState} from "react";
 
 const PrimaryAppBar = () => {
+    const [sideMenu, setSideMenu] = useState(false);
     const theme = useTheme();
+
+    const isScreenSmall = useMediaQuery(theme.breakpoints.up("sm"))
+
+    useEffect(() => {
+        if(isScreenSmall && sideMenu){
+            setSideMenu(false);
+        }
+    }, [isScreenSmall]);
+
+    const toggleDrawer = (open: boolean) =>
+        (event: React.MouseEvent) => {
+        setSideMenu(open);
+    }
 
     return(
         <AppBar position={"fixed"} sx={{
+            zIndex:(theme) => theme.zIndex.drawer + 2,
             backgroundColor: theme.palette.background.default,
             borderBottom: `1px solid ${theme.palette.divider}`,
         }}>
@@ -13,6 +30,24 @@ const PrimaryAppBar = () => {
                 height: theme.primaryAppBar.height,
                 minHeight: theme.primaryAppBar.height,
             }}>
+
+                <Box sx={{ display:{xs:"block", sm:"none"} }}>
+                    <IconButton color="inherit"
+                                aria-label="open drawer"
+                                edge="start"
+                                onClick={toggleDrawer(true)}
+                                sx={{mr:2}}>
+                        <MenuIcon/>
+                    </IconButton>
+                </Box>
+
+                <Drawer anchor={"left"} open={sideMenu} onClose={toggleDrawer(false)}>
+                    {[...Array(100)].map((_, i) => (
+                        <Typography key={i}>
+                            {i + 1}
+                        </Typography>
+                    ))}
+                </Drawer>
 
                 <Link href="/" underline={"none"} color={"inherit"}>
                     <Typography
