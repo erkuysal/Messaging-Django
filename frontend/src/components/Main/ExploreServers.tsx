6c531803218b1500747626b54b6e8bd66277d1c3
Feyzullah <f.erkuysal@gmail.com>
2024-08-23 12:20:41 +0300

    Made Stack Menu Functional

--- frontend/src/components/Main/ExploreServers.tsx
+++ frontend/src/components/Main/ExploreServers.tsx
@@ -24,6 +24,7 @@ interface Server {
     description: string;
     icon: string;
     category: string;
+    banner: string;
 }
 
 
@@ -40,10 +41,20 @@ const ExploreServer = () => {
     return <>
         <Container maxWidth="lg">
             <Box sx={{ pt:1 }}>
-                <Typography variant="h3" noWrap component="h2" color="textSecondary"
-                            sx={{display:{sm:"block", fontWeight:500, fontSize:"48px", letterSpacing:"-1px"},
-                            textAlign:{xs :"center", sm:"left"}
-                            }}>
+                <Typography
+                    variant="h3"
+                    noWrap
+                    component="h2"
+                    color="textSecondary"
+                    sx={{display:
+                            {
+                            sm:"block",
+                            fontWeight:500, fontSize:"48px",
+                            letterSpacing:"-1px", textTransform: "capitalize"
+                            },
+                        textAlign:{xs :"center", sm:"left"},
+                        }
+                }>
                     { categoryName
                         ? `All About ${categoryName}`
                         : "Were you looking for something?"}
@@ -57,16 +68,59 @@ const ExploreServer = () => {
             <Grid container spacing={{xs: 0 , sm: 2}}>
                 {dataCRUD.map((item) => (
                     <Grid item key={item.id} xs={12} sm={6} md={6} lg={3}>
-                        <Card sx={{height:"100%", display: "flex", flexDirection:"column",
-                        backShadow:"none", backgroundImage: "none",
-                        }}>
+                        <Card
+                            sx={
+                                {
+                                    height:"100%", display: "flex", flexDirection:"column",
+                                    backShadow:"none", backgroundImage: "none",  borderRadius: 0,
+                                }
+                            }
+                        >
 
                             <Link to={`/server/${item.id}`} style={{ textDecoration:"none", color:"inherit"}}>
                                 <CardMedia component="img"
-                                           image="https://source.unsplash.com/random/"
-                                           alt="random image"
-                                           sx={{display: {xs:"none", sm:"block"}}}
-                                ></CardMedia>
+                                   image={
+                                        item.banner
+                                            ? `${MEDIA_URL}${item.banner}`
+                                            : "https://source.unsplash.com/random"
+                                   }
+                                   alt="random image"
+                                   sx={{display: {xs:"none", sm:"block"}}}
+                                />
+
+                                <CardContent sx={{flexGrow: 1, p:0, "&:last-child": {paddingBottom: 0}}}>
+                                    <List>
+                                        <ListItem disablePadding>
+
+                                            <ListItemIcon sx={{minWidth: 0}}>
+                                                <ListItemAvatar sx={{minWidth:"50px"}}>
+                                                    <Avatar alt={"server Icon"} src={`${MEDIA_URL}${item.icon } `}/>
+                                                </ListItemAvatar>
+                                            </ListItemIcon>
+
+                                            <ListItemText
+                                                primary= {
+                                                <Typography varient="body2"
+                                                            textAlign="start"
+                                                               sx={{
+                                                                   fontWeight: 500,
+                                                                   textOverflow:"ellipsis",
+                                                                   overflow: "hidden",
+                                                                   whiteSpace: "nowrap",
+                                                               }}>
+                                                    {item.name}
+                                                </Typography>
+                                                }
+                                                secondary={
+                                                <Typography variant="body2">
+                                                    {item.category}
+                                                </Typography>
+                                                }
+                                            />
+
+                                        </ListItem>
+                                    </List>
+                                </CardContent>
                             </Link>
                         </Card>
                     </Grid>
